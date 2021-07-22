2f805b83c554232bf0cc73c61a7de168b6126bbc
NicoleJoseph18 <nicolejosephk64581@gmail.com>
2021-07-22 18:41:08 +0000

    error added

--- public/js/writeMessage.js
+++ public/js/writeMessage.js
@@ -2,11 +2,23 @@ const onSubmit = () => {
     
     const passcodeInput = document.querySelector("#passcode").value;
     const messageInput = document.querySelector("#message").value;
+    try{
+
+    
     const payload = {
+
         passcode: passcodeInput   ,
         message: messageInput
 
     }
-    
+    if( payload.message.length >  6)
+    {
+        throw "the message is too long, stay in a length of 6"
+    }
     firebase.database().ref().push(payload)
+}
+catch(errorMessage){
+errorText.innerHTML = errorMessage
+return
+}
 }
