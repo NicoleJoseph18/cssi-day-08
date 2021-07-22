511f09b8a8447689849d0030a34cbc5d7b969a80
NicoleJoseph18 <nicolejosephk64581@gmail.com>
2021-07-22 17:07:25 +0000

    viewing messages working

--- public/js/viewMessages.js
+++ public/js/viewMessages.js
@@ -3,7 +3,13 @@ const getMessages = () =>{
     const messagesRef = firebase.database().ref();
     messagesRef.on('value',(snapshot) => {
         const data = snapshot.val()
-        console.log(data)
+        for( let key in data){
+            if(key === passcode.value){
+                const message = document.querySelector("#message")
+                message.innerHTML = data[key]
+            console.log("match found")
+            }
+        }
     } )
 }
 
